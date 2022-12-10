c6c0c008ff6150758ff04dabf029fe795a760359
Lutin <tclutin01@gmail.com>
2022-12-11 00:02:03 +0500

    Completed the task

--- Program.cs
+++ Program.cs
@@ -2,9 +2,29 @@
 {
     class Program
     {
+        private static string save;
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Example for double: 1,1, 2,2");
+            for (; ; )
+            {
+                string input = Console.ReadLine();
+
+                if (input == "q") break;
+
+                if (int.TryParse(input, out int result))
+                {
+                    Console.WriteLine((char)result);
+                }
+                else
+                {
+                    if (Convert.ToDouble(input) == Convert.ToDouble(save))
+                    {
+                        break;
+                    }
+                }
+                save = input;
+            }
         }
     }
 }
